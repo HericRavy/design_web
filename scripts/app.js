00fa50f6cea4fc69dc521282f7261b4c5d764b42
HericRavy <heric.ravy@escolar.ifrn.edu.br>
2026-08-19 18:00:34 -0300

    Terminar Depois

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
 alert("Olá Mundo!");
+
+var nasc = 2009;
+let nome = "Heric"
+const viva = true;
+let alura = 1.76;
+
+if (viva){
+    let saudacao = "Olá" + nome + "!"
+    let msg `Altura ${altura}m | Idade ${2026-nasc}`;
+    alert(saudacao )
+}
