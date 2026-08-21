a6bee6c2e1d6f8af9db04932c6c27db1306fcc4e
HericRavy <heric.ravy@escolar.ifrn.edu.br>
2026-08-21 14:13:42 -0300

    Calculando Idade

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+var nasc = 2009;
+let nome = "Heric";
+const viva = true
+
+function calcIdade(ano=2026){
+    let idade = ano - nasc;
+    alert(`Dentro de Função - Idade ${idade}`);
+    return idade;
+}
+calcIdade();
+//alert(`Fora da função: Idade ${idade}`); Erro pois a variável lrt não existe fora do escopo dela
+alert(`Fora da função: Idade ${calcIdade(2027)}`);
