a2c7bc02bd7706b833c04e95f02bbc92540fe3d8
HericRavy <heric.ravy@escolar.ifrn.edu.br>
2026-09-04 14:27:03 -0300

    Testando Continue e Break

--- Js/app4.js
+++ Js/app4.js
@@ -1,4 +1,12 @@
 let vezes = Number(prompt("Digite o número de vezes: "));
 for (let i = 0; i <= vezes; i++){
-    alert(`Contei ${i}`);
+    if (vezes > 100){
+        alert("Valor muito alto, digite um valor de 0 a 100")
+        break
+    }
+    alert(`Contei ${i}`); //É ímpar?
+    if (i%2!==0){ //Se ímpar, continua = voltar para o ínicio
+        continue
+    }
+    alert(`${i} é par`)
 }
